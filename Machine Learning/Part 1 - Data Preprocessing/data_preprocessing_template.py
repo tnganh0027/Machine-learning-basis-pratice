965e81ed4b44e9999ea1322b4e4efeda97cc314a
tnganh0027 <tnganh0027@gmail.com>
2018-03-14 09:19:52 +0700

    take care the missing data

--- Machine Learning/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -8,12 +8,15 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
+X
 y = dataset.iloc[:, 3].values
+y
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
+#missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis=0)
+imputer = imputer.fit(X[:, 1:3])
+    
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
